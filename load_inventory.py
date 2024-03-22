d66baa8071dba5814c489325a790703c9b6df7ec
maheen-raza <113572260+maheen-raza@users.noreply.github.com>
2024-03-22 00:36:00 +0000

    fighting for my life good grief

--- load_inventory.py
+++ load_inventory.py
@@ -1,4 +1,4 @@
-from ansible_runner import run
+from ansible_runner.interface import run_command
 import yaml
 
 with open("hosts.yml") as f: # open the inventory file "hosts.yml" in our directory
@@ -9,5 +9,6 @@ for group, hosts in inventory_file.items():
     for host, attrs in hosts.get('hosts', {}).items(): # find the hosts allocated to said group
         print(f"  Name: {host}, IP Address: {attrs.get('ansible_host')}") # print out the hosts and their IP addreses that belong to that specific group
 
-playbook_content = [{'hosts': 'all', 'tasks': [{'ping': {}}]}]
-run(inventory=inventory_file, playbook=playbook_content)
+result = run_command("ansible all:localhost -m ping") # using the run_command helper function in the ansible runner documentation to run the command that is given to ping all hosts
+print("Results after pinging hosts:\n")
+print(result) # printing the results when you ping the hosts
